66632e3f43aa9971290a3b34ce59afe1c885f55a
Song.gunho <song6497@gmail.com>
2019-04-17 22:01:14 +0900

    Add test description annotation

--- src/test/java/com/ghsong/restapi/events/EventControllerTests.java
+++ src/test/java/com/ghsong/restapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.ghsong.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ghsong.restapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +39,7 @@ public class EventControllerTests {
     //EventRepository eventRepository;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
 
         EventDto event = EventDto.builder()
@@ -71,6 +73,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad() throws Exception {
 
         Event event = Event.builder()
@@ -102,6 +105,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -112,6 +116,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("spring")
