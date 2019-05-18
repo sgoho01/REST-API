0d744e1eeff388ad85f763c3783b84f898946d3f
ghsong <ghsong@gmail.com>
2019-05-18 15:43:02 +0900

    Config authentication server

---
+++ src/test/java/com/ghsong/restapi/configs/AuthServerConfigTest.java
@@ -0,0 +1,51 @@
+package com.ghsong.restapi.configs;
+
+import com.ghsong.restapi.accounts.Account;
+import com.ghsong.restapi.accounts.AccountRole;
+import com.ghsong.restapi.accounts.AccountService;
+import com.ghsong.restapi.common.BaseControllerTest;
+import com.ghsong.restapi.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthServerConfigTest extends BaseControllerTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Test
+    @TestDescription("인증을 토큰을 발급 받는 테스트")
+    public void getAuthToken() throws Exception {
+        // Given
+        String username = "ghsong2@email.com";
+        String password = "ghsong";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountService.saveAccount(account);
+
+        String clientId = "myApp";
+        String clientSecret = "pass";
+
+        this.mockMvc.perform(post("/oauth/token")
+                    .with(httpBasic(clientId, clientSecret))
+                    .param("username", username)
+                    .param("password", password)
+                    .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+
+
+}
